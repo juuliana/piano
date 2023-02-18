5bae3fa2dc34e5dd49f1afc39d3c10f3a885dafb
juuliana <jumaju10@hotmail.com>
2023-02-18 19:02:43 -0300

    feat: create structure

---
+++ src/utils/structure.ts
@@ -0,0 +1,86 @@
+import { Codes, Notes, SharpNotes, Keys } from "./notes";
+
+interface Structure {
+  code: Codes;
+  name: Notes | SharpNotes | string;
+  key: Keys;
+  sound: string;
+}
+
+export const structure: Structure[] = [
+  //notes
+  {
+    code: "A",
+    name: "lá",
+    key: "h",
+    sound: "/sounds/A.mp3",
+  },
+  {
+    code: "B",
+    name: "sí",
+    key: "j",
+    sound: "/sounds/B.mp3",
+  },
+  {
+    code: "C",
+    name: "dó",
+    key: "a",
+    sound: "/sounds/C.mp3",
+  },
+  {
+    code: "D",
+    name: "ré",
+    key: "s",
+    sound: "/sounds/D.mp3",
+  },
+  {
+    code: "E",
+    name: "mí",
+    key: "d",
+    sound: "/sounds/E.mp3",
+  },
+  {
+    code: "F",
+    name: "fá",
+    key: "f",
+    sound: "/sounds/F.mp3",
+  },
+  {
+    code: "G",
+    name: "sol",
+    key: "g",
+    sound: "/sounds/G.mp3",
+  },
+
+  //sharp notes
+  {
+    code: "C#",
+    name: "dó#",
+    key: "w",
+    sound: "/sounds/CSharp.mp3",
+  },
+  {
+    code: "D#",
+    name: "ré#",
+    key: "e",
+    sound: "/sounds/DSharp.mp3",
+  },
+  {
+    code: "F#",
+    name: "fá#",
+    key: "t",
+    sound: "/sounds/FSharp.mp3",
+  },
+  {
+    code: "G#",
+    name: "sol#",
+    key: "y",
+    sound: "/sounds/GSharp.mp3",
+  },
+  {
+    code: "A#",
+    name: "lá#",
+    key: "u",
+    sound: "/sounds/ASharp.mp3",
+  },
+];
