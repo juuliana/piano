c85bdefda71d21a1ab7dd3239cb28ddec00fd0ee
juuliana <jumaju10@hotmail.com>
2023-02-18 19:02:29 -0300

    feat: create notes

---
+++ src/utils/notes.ts
@@ -0,0 +1,28 @@
+export type Notes = "dó" | "ré" | "mí" | "fá" | "sol" | "lá" | "sí";
+export type SharpNotes = "dó#" | "ré#" | "fá#" | "sol#" | "lá#";
+export type Codes =
+  | "A"
+  | "B"
+  | "C"
+  | "D"
+  | "E"
+  | "F"
+  | "G"
+  | "C#"
+  | "D#"
+  | "F#"
+  | "G#"
+  | "A#";
+export type Keys =
+  | "a"
+  | "s"
+  | "d"
+  | "f"
+  | "g"
+  | "h"
+  | "j"
+  | "w"
+  | "e"
+  | "t"
+  | "y"
+  | "u";
