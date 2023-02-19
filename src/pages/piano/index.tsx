b1bc42021c5c7f8eb0bc0caf948b2fdf4788dd9b
juuliana <jumaju10@hotmail.com>
2023-02-19 14:52:40 -0300

    feat: add labelOptions and toggle

--- src/pages/piano/index.tsx
+++ src/pages/piano/index.tsx
@@ -1,23 +1,48 @@
+import { useState } from "react";
+
+import { IToggle } from "@/src/components/toggle/types";
 import { useKeydown } from "@/src/hooks";
-import { Key, MajorKey } from "@/src/components";
+import { Key, MajorKey, Toggle } from "@/src/components";
 import { notesStructure, sharpNotesStructure } from "@/src/utils";
 
-import { Container, FirstContent, SecondContent } from "./styles";
+import {
+  Configuration,
+  Container,
+  FirstContent,
+  SecondContent,
+} from "./styles";
 
 export function Piano() {
+  const [labelOption, setLabelOption] = useState<IToggle.Selected>("hidden");
+
   const { typed } = useKeydown();
 
   return (
     <Container>
+      <Configuration>
+        <Toggle onSelect={setLabelOption} />
+      </Configuration>
+
       <FirstContent>
-        {notesStructure.map(({ code }) => (
-          <Key key={code} note={code} typed={typed} />
+        {notesStructure.map((note) => (
+          <Key
+            key={note.key}
+            note={note}
+            typed={typed}
+            labelOption={labelOption}
+          />
         ))}
       </FirstContent>
 
       <SecondContent>
-        {sharpNotesStructure.map(({ code }) => (
-          <MajorKey empty={!code} key={code} note={code} typed={typed} />
+        {sharpNotesStructure.map((note) => (
+          <MajorKey
+            empty={!note.code}
+            key={note.key}
+            note={note}
+            typed={typed}
+            labelOption={labelOption}
+          />
         ))}
       </SecondContent>
     </Container>
--- src/pages/piano/styles.ts
+++ src/pages/piano/styles.ts
@@ -3,20 +3,29 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   cursor: pointer;
+  position: relative;
+  border-radius: 10px;
+  background: #191919;
+  padding-bottom: 10px;
+`;
+
+export const Configuration = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100px;
+  position: absolute;
+  margin: 15px 15px;
+  border-radius: 10px;
 `;
 
 export const FirstContent = styled.div`
   display: flex;
-  position: relative;
   flex-direction: row;
   justify-content: center;
   align-items: flex-end;
   gap: 10px;
-  width: 650px;
-  height: 300px;
-  border-radius: 10px;
-  background: #191919;
-  padding-bottom: 10px;
+  width: 860px;
+  height: 280px;
 `;
 
 export const SecondContent = styled.div`
@@ -25,7 +34,7 @@ export const SecondContent = styled.div`
   display: flex;
 
   flex-direction: row;
-  gap: 35px;
+  gap: 25px;
   background: transparent;
-  margin: 65px 72px;
+  margin: 125px 52px;
 `;
